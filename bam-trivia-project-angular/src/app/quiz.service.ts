61d15ac767d25c665d03e378b814c8b9251337b8
Andy Ly <andywindsong@gmail.com>
2019-03-28 01:05:01 -0500

    got the quiz list page to display, but not data from api

--- bam-trivia-project-angular/src/app/quiz.service.ts
+++ bam-trivia-project-angular/src/app/quiz.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from "rxjs/internal/operators";
+
 import Quiz from './models/quiz';
 
 @Injectable({
@@ -14,12 +16,31 @@ export default class QuizService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Array<Quiz>> {
-    return this.http.get<Array<Quiz>>(this.QUIZZES_API);
+    return this.http.get<Array<Quiz>>(this.QUIZZES_API)
+      .pipe(
+        catchError(this.handleError)
+      );;
   }
 
   get(id: string) {
     return this.http.get(`${this.QUIZZES_API}/${id}`);
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
   // can add other crud functionality later like add
 }
