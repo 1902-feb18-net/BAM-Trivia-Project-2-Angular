b1b6e0c9f903385b4e8fced79ddbd62b38e737d1
Impstar <magesbe@gmail.com>
2019-03-28 20:40:36 -0500

    the take-quiz.html will not display questions!  Not finished setting that up of course, but way better than before

--- bam-trivia-project-angular/src/app/app-routing.module.ts
+++ bam-trivia-project-angular/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnswerListComponent } from './answer-list/answer-list.component';
+import { TakeQuizComponent } from './take-quiz/take-quiz.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'quiz-list', component: QuizListComponent },
   { path: 'answers', component: AnswerListComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'take-quiz', component: TakeQuizComponent }
 
 ];
 
