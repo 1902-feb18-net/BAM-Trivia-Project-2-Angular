590817b8d0a90ffb1b827c3675f336417ca6ffec
Impstar <magesbe@gmail.com>
2019-03-28 21:31:04 -0500

    we got the answers for the question to appear, now to add buttons to it

--- bam-trivia-project-angular/src/app/take-quiz/take-quiz.component.ts
+++ bam-trivia-project-angular/src/app/take-quiz/take-quiz.component.ts
@@ -16,11 +16,11 @@ import { elementStyleProp } from '@angular/core/src/render3';
   styleUrls: ['./take-quiz.component.css']
 })
 export class TakeQuizComponent implements OnInit {
-  quizzes: Quiz[];
-  questions: Questions[];
-  answers: Answer[];
+  quizzes: Quiz[] = [];
+  questions: Questions[] = [];
+  answers: Answer[] = [];
   answeredQuestion: string;
-  questionAnswers: Answer[];
+  questionAnswers: Answer[] = [];
   correctAnswer: Answer;
   givenQuestion: Questions;
   questionsAnswered: number;
@@ -41,22 +41,21 @@ export class TakeQuizComponent implements OnInit {
   getQuestions(quiz : Quiz) { this.questionsService.getQuestions(quiz).subscribe(data => {
     console.log(data);
     this.questions = data;
-    if (this.questions != null && this.questions != undefined)
     this.givenQuestion = this.questions[0];
-  else
-    console.log("given questions has not triggered possibly because it's async");
+    this.getAnswers(1);
     }, err => console.log(err));}
 
   getAnswers(quizId: number) {
     this.answerService.getAnswers(quizId).subscribe(data => {
       console.log(`answers: ${data}`);
       this.answers = data;
+      this.getQuestionAnswers(this.questions[0]);
     }, err => console.log(err));
   }
 
   startQuiz() {
      this.getQuestions(this.quizzes[0]);
-     this.getAnswers(1);
+     //this.getAnswers(1);
 
      this.questionsAnswered = 0;
      this.numberOfCorrectAnswers = 0;
@@ -79,13 +78,13 @@ export class TakeQuizComponent implements OnInit {
 
   getCorrectAnswer(question: Questions): Answer {
 
-    //This loops goes through a list of all answers received and selects
-    //  the ones that apply to the given question only
-    for (var i = 0; i < this.answers.length; i++)
-    {
-      if (this.answers[i].id == question.id)
-        this.questionAnswers.push(this.answers[i]);
-    }
+    // //This loops goes through a list of all answers received and selects
+    // //  the ones that apply to the given question only
+    // for (var i = 0; i < this.answers.length; i++)
+    // {
+    //   if (this.answers[i].id == question.id)
+    //     this.questionAnswers.push(this.answers[i]);
+    // }
 
     //This loops through the answers for the given question and returns
     //  the correct answer
@@ -95,6 +94,21 @@ export class TakeQuizComponent implements OnInit {
         this.correctAnswer = this.answers[i];
     }
       return this.correctAnswer;
+  }
+
+  getQuestionAnswers(question: Questions) {
+    for (var i = 0; i < this.answers.length; i++)
+    {
+      console.log(`the for loop has triggered, answers[${i}].questionId == ${this.answers[i].questionId}`)
+      if (this.answers[i].questionId === question.id)
+      {
+        this.questionAnswers.push(this.answers[i]);
+        console.log("the if statement has triggered, it should trigger 4 times");
+      }
+        
+    }
+    console.log("getQuestionAnswers has triggered");
+    console.log(this.questionAnswers)
   }
 
   checkAnswer(): boolean {
